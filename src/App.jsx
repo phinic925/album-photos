1279582edb21291e3b06fb5263e8781736a39870
phinic925 <phinicnyangate480@gmail.com>
2023-01-29 14:40:10 +0300

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
+import "./App.css";
+import Register from "./components/auth/Register";
+import Home from "./components/Home";
 
 function App() {
-
   return (
     <div className="App">
-   
+      {/* <Register />
+      <Home/> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
