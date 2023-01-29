6957f4e91c0d1221e50c2714833ae3d246b4e04b
phinic925 <phinicnyangate480@gmail.com>
2023-01-29 14:12:57 +0300

    deletes files

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Home from "./components/Home";
 function App() {
   return (
     <div className="App">
-      <Register />
-      <Home/>
+      {/* <Register />
+      <Home/> */}
     </div>
   );
 }
